0799646e2c8d015474daa40a737e1fb7a987db1e
Fırat Kaya <48600092+benfiratkaya@users.noreply.github.com>
2023-09-05 15:17:58 +0300

    added set nickname feature

--- modules/roleSyncing.js
+++ modules/roleSyncing.js
@@ -12,15 +12,15 @@ module.exports = async (client) => {
       // If member doesn't get synced role, do nothing
       if (!newMemberData.roles.cache.has(client.settings.syncedRoleID)) return;
 
-      // Get user data
-      const userInfo = await api.getUserInfo(newMemberData.user.id);
-
-      // If user is not synced on website, do nothing
-      if (!userInfo) return;
-
       // If user is synced on website and doesn't exist in cache, add user to cache
       const userDataCache = await userData.get(newMemberData.user.id);
       if (!userDataCache) {
+        // Get user data
+        const userInfo = await api.getUserInfo(newMemberData.user.id);
+
+        // If user is not synced on website, do nothing
+        if (!userInfo) return;
+
         // Add user to cache
         userData.add(newMemberData.user.id, {
           id: userInfo.id,
@@ -30,6 +30,10 @@ module.exports = async (client) => {
         userInfo.roles
           .filter((role) => role.discordRoleID)
           .forEach((role) => newMemberData.roles.add(role.discordRoleID));
+
+        if (client.settings.setNicknameStatus) {
+          newMemberData.setNickname(userInfo.realname);
+        }
       }
     }
   });
@@ -45,7 +49,10 @@ module.exports = async (client) => {
       if (newMemberData.roles.cache.has(client.settings.syncedRoleID)) return;
 
       // Get user data
-      const userDataCache = await userData.get(member.user.id);
+      const userDataCache = await userData.get(newMemberData.user.id);
+
+      // If user is not cached, do nothing
+      if (!userDataCache) return;
 
       // Remove website roles from member
       const userRoles = await api.getUserRoles(userDataCache.id);
@@ -54,27 +61,26 @@ module.exports = async (client) => {
         .forEach((role) => newMemberData.roles.remove(role.discordRoleID));
 
       // Remove user from cache
-      userData.delete(member.user.id);
+      userData.delete(newMemberData.user.id);
+
+      if (client.settings.setNicknameStatus) {
+        newMemberData.setNickname('');
+      }
     }
   });
 
   // Give synced role to member which is synced on website
   client.on('guildMemberAdd', async (member) => {
-    // Check if user is synced in cache
-    const userDataCache = await userData.get(member.user.id);
+    // Get user data
+    const userInfo = await api.getUserInfo(member.user.id);
 
-    // Check if user is synced on website
-    if (!userDataCache) {
-      const userInfo = await api.getUserInfo(member.user.id);
+    // If user is not synced on website, do nothing
+    if (!userInfo) return;
 
-      // If user is not synced on website, do nothing
-      if (!userInfo) return;
-
-      // If user is synced on website, add user to cache
-      userData.add(member.user.id, {
-        id: userInfo.id,
-      });
-    }
+    // If user is synced on website, add user to cache
+    userData.add(member.user.id, {
+      id: userInfo.id,
+    });
 
     // If everything is ok, give synced role to member
     member.roles.add(client.settings.syncedRoleID);
