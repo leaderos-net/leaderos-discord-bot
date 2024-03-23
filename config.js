5d473b63452a21c71377332705bce0ea6172b6e0
pwr239 <85400163+pwr239@users.noreply.github.com>
2024-03-23 23:43:39 +0300

    config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  botToken: process.env.DISCORD_BOT_TOKEN ?? 'YOUR_BOT_TOKEN',
+  botToken: process.env.DISCORD_BOT_TOKEN ?? 'MTE1MTIyNjY4Mjg4NDIzMTIxOQ.Guimz7.7zWuM9tRVJNc_cOPBz7hv6vtBQrMEX1Xm-jFlY',
   websiteUrl: process.env.WEBSITE_URL ?? 'https://yourwebsite.com/',
   apiKey: process.env.API_KEY ?? 'YOUR_API_KEY',
   language: process.env.LANGUAGE ?? 'en',
