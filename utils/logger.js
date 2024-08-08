836502409e7524a36756e00105a432e3375ed1a5
Fırat Kaya <48600092+benfiratkaya@users.noreply.github.com>
2024-08-08 23:31:12 +0300

    fixed logger function

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,14 @@
 const config = require('../config.js');
 
 module.exports = (message) => {
-  if (!config.debug) return;
+  if (
+    !config.debug ||
+    config.debug === undefined ||
+    config.debug === false ||
+    config.debug === 'false'
+  ) {
+    return false;
+  }
 
   const date = new Date();
   console.log(`${date} [BOT]: ${message}`);
